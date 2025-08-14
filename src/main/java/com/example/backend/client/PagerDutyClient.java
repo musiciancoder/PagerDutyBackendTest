a0f0bd57d0e04d04005b913d19604044d06f3a27
musiciancoder <andresguitarra1980@gmail.com>
2025-08-13 21:42:50 -0400

    token test ok con logger

--- src/main/java/com/example/backend/client/PagerDutyClient.java
+++ src/main/java/com/example/backend/client/PagerDutyClient.java
@@ -1,5 +1,7 @@
 package com.example.backend.client;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
@@ -10,12 +12,14 @@ public class PagerDutyClient {
 
     private final RestTemplate restTemplate;
     private final HttpHeaders headers;
+    private final Logger logger = LoggerFactory.getLogger(PagerDutyClient.class);
 
     public PagerDutyClient(@Value("${pagerduty.api.token}") String apiToken) {
         this.restTemplate = new RestTemplate();
         this.headers = new HttpHeaders();
         headers.set("Authorization", "Token token=" + apiToken);
         headers.set("Accept", "application/vnd.pagerduty+json;version=2");
+        logger.info("PagerDutyClient initialized with provided API token.");
     }
 
 /*    public static void prueba() {
